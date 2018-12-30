a6437a1ca12f87c92ad23e7d8da40746310cfd5a
ziggylineous <ignacio_ferrari@hotmail.com>
2018-12-30 16:50:00 -0300

    create item form

--- item_catalog/README.md
+++ item_catalog/README.md
@@ -4,6 +4,7 @@
 1. Create a python 3 environment and install with pip:
 - flask
 - flask-sqlalchemy
+- flask-wtf
 
 2. Set the `FLASK_APP` environment variable to `item_catalog.py`
 3. database
--- item_catalog/app/routes.py
+++ item_catalog/app/routes.py
@@ -1,9 +1,51 @@
 # the first app is the package,
 # the second is the Flask application
-from app import app
+from app import app, db
+from app.models import User, Item
+from flask import render_template, request, flash, url_for, redirect
+from app.forms import NewItemForm
 
 @app.route('/')
-@app.route('/index')
 @app.route('/items')
-def home():
-    return 'test ok!'
+def show_items():
+    return render_template(
+        'items.html',
+        items=Item.query.all()
+    )
+
+
+@app.route('/items/<string:item_name>')
+def view_item(item_name):
+    
+
+
+@app.route('/items/new', methods=['GET', 'POST'])
+def new_item():
+    form = NewItemForm()
+    
+    if form.validate_on_submit():
+        item = create_item(
+            form.name.data,
+            form.description.data
+        )    
+        flash('Created new item: {}'.format(item.name))
+        return redirect(url_for('show_items'))
+    
+    else:
+        return render_template(
+            'new-item.html',
+            title='Create New Item',
+            form=form
+        )
+
+
+def create_item(name, description):
+    item = Item(
+        name=name,
+        description=description
+    )
+        
+    db.session.add(item)
+    db.session.commit()
+
+    return item
